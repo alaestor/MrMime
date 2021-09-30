d68d03087c9221c669722627b9a0c7892796a040
Alaestor <alaestor@null.net>
2021-09-29 23:24:07 -0400

    cleaned up example, improved statistic printout

--- src/example.cpp
+++ src/example.cpp
@@ -65,66 +65,31 @@ void example_scan_directory(const fs::path folder_path)
 	}
 }
 
-constexpr char size_byte_arrays[]{
-	"\xff\xd8"
-	"GIF87a"
-	"GIF89a"
-	"\x89PNG" "IHDR" "acTL"
-	"\x89PNG"
-	"WEBP"
-	"II*\x00'"
-	"MM\x00*"
-	"BM"
-	"\x00\x00\x01\x00"
-	"\x00\x00\x02\x00"
-	"CWS"
-	"FWS"
-	"ZWS"
-	"FLV"
-	"%PDF"
-	"8BPS\x00\x01"
-	"8BPS\x00\x02"
-	"CSFCHUNK"
-	"PK\x03\x04"
-	"PK\x05\x06"
-	"PK\x07\x08"
-	"7z\xBC\xAF\x27\x1C"
-	"\x52\x61\x72\x21\x1A\x07\x00"
-	"\x52\x61\x72\x21\x1A\x07\x01\x00"
-	"hydrus encrypted zip"
-	"ftypmp4"
-	"ftypisom"
-	"ftypM4V"
-	"ftypMSNV"
-	"ftypavc1"
-	"ftypFACE"
-	"ftypdash"
-	"ftypqt"
-	"fLaC"
-	"RIFF"
-	"WAVE"
-	"AVI"
-	"\x30\x26\xB2\x75\x8E\x66\xCF\x11" "\xA6\xD9\x00\xAA\x00\x62\xCE\x6C"
-};
-
-constexpr std::size_t size_skip_bytes{ 8+8+8+21+4+4+4+4+4+4+4+4+8 };
-
-constexpr std::size_t raw_size{ sizeof(size_byte_arrays) + size_skip_bytes };
-
 int main()
 {
 	example_scan_directory("test_dump");
 
+	// bellow here is just a print-out of some internal Mr.Mime statistics
+
 	using
+		MrMime::internal::signatures,
 		MrMime::internal::Signatures_t,
 		MrMime::internal::signature_count,
 		MrMime::internal::size_of_largest_signature;
 
+	constexpr std::size_t size_including_skipped_bytes{
+		std::apply(
+			[]<typename ... SIGS>(const SIGS& ... sigs)
+				constexpr -> std::size_t { return (sigs.size() + ...); },
+			signatures
+		)
+	};
+
 	cout
 		<< "\n\t" << signature_count << " signatures defined, "
 		<< " consuming " << sizeof(Signatures_t) << " bytes "
-		<< "(raw: " << raw_size << " bytes)\n"
-		<< "\tThe largest signature (and the file readbuffer) is "
+		<< "(matches to " << size_including_skipped_bytes << " bytes)\n"
+		<< "\tThe largest signature (and the header_data_buffer_t) is "
 		<< size_of_largest_signature << " bytes\n"
 		<< endl;
 
