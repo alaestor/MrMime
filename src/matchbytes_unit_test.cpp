ea163a7714ba069fc7f427411502017bf9963630
Alaestor <alaestor@null.net>
2021-10-19 03:56:35 -0400

    improved cursor test

--- src/matchbytes_unit_test.cpp
+++ src/matchbytes_unit_test.cpp
@@ -3,14 +3,14 @@
 using namespace MrMime::internal;
 
 /// MatchBytes Unit Test
-static_assert(	
+static_assert(
 	[]() consteval -> bool
 	{
 		const auto mb_0{ static_cast<std::byte>(0x10) };
 		const auto mb_1{ static_cast<std::byte>(0xFF) };
 		const auto mb_2{ static_cast<std::byte>('Z') };
-		constexpr std::array arr_succ{ mb_0, mb_1, mb_2 };
-		constexpr std::array arr_fail{ mb_0, mb_2, mb_1 };
+		constexpr std::array arr_succ{ mb_0, mb_1, mb_2, mb_0 };
+		constexpr std::array arr_fail{ mb_0, mb_2, mb_1, mb_0 };
 
 		constexpr MatchBytes mb("\x10\xFFZ");
 
@@ -32,7 +32,7 @@ static_assert(
 			{
 				auto cursor{ arr_succ.begin() };
 				(void) mb.matches(cursor);
-				return  cursor == arr_succ.end();
+				return cursor == arr_succ.begin() + mb.size();
 			}()
 		);
 
